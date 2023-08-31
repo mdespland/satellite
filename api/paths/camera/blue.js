e9c3be1285ac60a16f97b5419b3830b85a9f4c2e
Marc Despland <marc.despland@orange.com>
2023-08-31 13:56:03 +0200

    all color mode

--- api/paths/camera/blue.js
+++ api/paths/camera/blue.js
@@ -16,7 +16,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-var mode="nofilter";
+var mode="blue";
 
 module.exports = function(CameraService){
     let operations = {
