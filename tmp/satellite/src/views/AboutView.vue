afdac8399de8b19a466ce8265cff3d4648a554e9
Despland Marc <marc.despland@orange.com>
2023-09-05 16:07:53 +0200

    grille

--- tmp/satellite/src/views/AboutView.vue
+++ tmp/satellite/src/views/AboutView.vue
@@ -1,15 +1,43 @@
+<script setup>
+
+import { onMounted } from 'vue'
+
+function drawLine(ctx, begin, end, stroke = 'black', width = 1) {
+    if (stroke) {
+        ctx.strokeStyle = stroke;
+    }
+
+    if (width) {
+        ctx.lineWidth = width;
+    }
+
+    ctx.beginPath();
+    ctx.moveTo(...begin);
+    ctx.lineTo(...end);
+    ctx.stroke();
+}
+
+
+
+onMounted(() => {
+  console.log(`the component is now mounted.`)
+  const canvas = document.querySelector('#canvas');
+if (canvas.getContext) {
+    const ctx = canvas.getContext('2d');
+    drawLine(ctx, [100, 150], [400, 150], 'black', 1);
+    for (var i=100;i<=400;i+=20) drawLine(ctx, [i, 50], [i, 250], 'black', 1);
+    for (var i=100;i<=400;i+=5) drawLine(ctx, [i, 100], [i, 200], 'black', 1);
+}
+})
+
+
+</script>
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<canvas id="canvas" height="400" width="500"></canvas>
 </template>
 
-<style>
-/*@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}*/
+<style scoped>
+#canvas {
+  background: white;
+}
 </style>
